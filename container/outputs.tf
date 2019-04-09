d72b4fc570559f47e0baade09e16eaadd560ef66
drumminganna <anna.renner1987@web.de>
2019-04-09 14:03:35 +0200

    small project

---
+++ container/outputs.tf
@@ -0,0 +1,7 @@
+# Output IP Adress and name of container
+output "ip" {
+  value = "${docker_container.container_id.ip_address}"
+}
+output "container_name" {
+  value = "${docker_container.container_id.name}"
+}
